57bb1d420e630e08f54ef3236add6d3ecc5037a2
 <korostelev560@gmail.com>
2026-05-26 07:02:36 +0300

    Добавление классов Contact и ContactManager

---
+++ contackManeger/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {PhoneNumber}";
+        }
+    }
+}
